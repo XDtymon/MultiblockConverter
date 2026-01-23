cb65938bcd705425ad7ddbc078d76cdff05fa886
XDtymon <100619534+XDtymon@users.noreply.github.com>
2026-01-23 17:53:17 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+const main_menu = [
+    "Menu",
+    "1 - Converter",
+    ""
+]
+
+const mods = [
+    "To universal from",
+    "1 - Custom Machinery",
+    "2 - GT"
+    
+]
+
+for (let i = 0; i < main_menu.length; i++) {
+    console.log(main_menu[i])
+}
+var output = prompt("Choice | ")
+switch (output) {
+    case 1:
+        
+    
+        
+        
+    
+}
